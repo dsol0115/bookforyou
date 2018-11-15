ba8db85001d91844bec304f7fbc449d5d1bc7acb
Dasol Yun <ddasoly@gmail.com>
2018-11-15 23:44:24 +0900

    Initial commit

---
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.administrator.myapplication;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    ConstraintLayout myLayout;
+    AnimationDrawable animationDrawable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myLayout=(ConstraintLayout)findViewById(R.id.myLayout);
+
+        animationDrawable=(AnimationDrawable) myLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(4500);
+        animationDrawable.setExitFadeDuration(4500);
+        animationDrawable.start();
+
+    }
+}
