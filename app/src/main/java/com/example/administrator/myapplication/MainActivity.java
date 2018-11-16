368b1bb1e0174139439c2a291ce776b5556245d9
Dasol Yun <ddasoly@gmail.com>
2018-11-16 15:47:40 +0900

    updated

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.administrator.myapplication;
 
+import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +24,11 @@ public class MainActivity extends AppCompatActivity {
         animationDrawable.setEnterFadeDuration(4500);
         animationDrawable.setExitFadeDuration(4500);
         animationDrawable.start();
+
+    }
+    public void onselectButtonClicked(View v){
+        Intent intent = new Intent(getApplicationContext(), ExselectActivity.class);
+        startActivity(intent);
 
     }
 }
