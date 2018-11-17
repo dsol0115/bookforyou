98ee0818cc23b3f3b567408da4733fe9afde9c18
Dasol Yun <ddasoly@gmail.com>
2018-11-17 14:19:17 +0900

    beforecardvieew

--- app/src/main/java/com/example/administrator/myapplication/ExselectActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/ExselectActivity.java
@@ -1,7 +1,9 @@
 package com.example.administrator.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ExselectActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class ExselectActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.exactivity_select);
+    }
+
+    public void onprefclicked(View v){
+        Intent myIntent = new Intent(getApplicationContext(), prefselectex.class);
+        startActivity(myIntent);
     }
 }
