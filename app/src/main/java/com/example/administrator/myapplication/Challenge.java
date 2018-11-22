8cb9b0e4bbc6855c4816cec287c9255145778498
mocaincoffee <wikid9375@gamil.com>
2018-11-22 17:48:47 +0900

    add file

--- app/src/main/java/com/example/administrator/myapplication/Challenge.java
+++ app/src/main/java/com/example/administrator/myapplication/Challenge.java
@@ -1,7 +1,10 @@
 package com.example.administrator.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Challenge extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class Challenge extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_challenge);
+
+        Button btn_go = (Button) findViewById(R.id.button);
+        btn_go.setOnClickListener(
+                new Button.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getApplicationContext(), info_challenge.class);
+                        startActivity(intent);
+                    }
+                }
+        );
     }
 }
