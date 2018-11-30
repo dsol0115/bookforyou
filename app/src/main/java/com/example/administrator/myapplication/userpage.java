1425173f367f5e227145c63749cb6cbce246480b
Dasol Yun <ddasoly@gmail.com>
2018-11-30 18:30:24 +0900

    ui design updated

--- app/src/main/java/com/example/administrator/myapplication/userpage.java
+++ app/src/main/java/com/example/administrator/myapplication/userpage.java
@@ -1,7 +1,9 @@
 package com.example.administrator.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class userpage extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class userpage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_userpage);
+    }
+
+    public void resultpref(View v){
+        Intent intent = new Intent(getApplicationContext(), resultprefselect.class);
+        startActivity(intent);
     }
 }
