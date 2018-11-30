311bad9728cd8afa3a7f3b4cd71e788317370849
mocaincoffee <wikid9375@gamil.com>
2018-11-30 14:56:10 +0900

    UI

--- app/src/main/java/com/example/administrator/myapplication/info_challenge.java
+++ app/src/main/java/com/example/administrator/myapplication/info_challenge.java
@@ -13,12 +13,12 @@ public class info_challenge extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_challenge);
 
-        Button btn_go = (Button) findViewById(R.id.button2);
+        Button btn_go = (Button) findViewById(R.id.button5);
         btn_go.setOnClickListener(
                 new Button.OnClickListener(){
                     @Override
                     public void onClick(View v) {
-                        Intent intent = new Intent(getApplicationContext(), bookcover.class);
+                        Intent intent = new Intent(getApplicationContext(), real_C.class);
                         startActivity(intent);
                     }
                 }
--- app/src/main/java/com/example/administrator/myapplication/pan_c_result.java
+++ app/src/main/java/com/example/administrator/myapplication/pan_c_result.java
@@ -1,28 +1,28 @@
 package com.example.administrator.myapplication;
 
-
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class real_C extends AppCompatActivity {
+public class pan_c_result extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_real__c);
+        setContentView(R.layout.activity_pan_c_result);
 
-        Button btn_go = (Button) findViewById(R.id.button4);
+        Button btn_go = (Button) findViewById(R.id.button12);
         btn_go.setOnClickListener(
                 new Button.OnClickListener(){
                     @Override
                     public void onClick(View v) {
-                        Intent intent = new Intent(getApplicationContext(), pan_c.class);
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                         startActivity(intent);
                     }
                 }
         );
+
     }
 }
