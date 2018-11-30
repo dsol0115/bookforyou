8654bd9725ba6c7dc67814d3d1757a5f4b484102
mocaincoffee <wikid9375@gamil.com>
2018-11-30 17:24:50 +0900

    camera

--- app/src/main/java/com/example/administrator/myapplication/real_C.java
+++ app/src/main/java/com/example/administrator/myapplication/real_C.java
@@ -2,17 +2,48 @@ package com.example.administrator.myapplication;
 
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.List;
 
 public class real_C extends AppCompatActivity {
+
+    Button btnCmera;
+    ImageView img;
+
+    Button Button_Camera;
+    ImageView imageView;
+
+    final int CAMERA_REQUEST_CODE = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_real__c);
+
+        btnCmera = (Button) findViewById(R.id.button2);
+        img = (ImageView) findViewById(R.id.imageView6);
+
+        btnCmera.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if(IsCameraAvailable()){
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, CAMERA_REQUEST_CODE);
+                }
+            }
+        });
+
+
 
         Button btn_go = (Button) findViewById(R.id.button4);
         btn_go.setOnClickListener(
@@ -24,5 +55,24 @@ public class real_C extends AppCompatActivity {
                     }
                 }
         );
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode==CAMERA_REQUEST_CODE){
+            Bundle bundle = data.getExtras();
+            Bitmap bitmap = (Bitmap)bundle.get("data");
+            img.setImageBitmap(bitmap);
+        }
+    }
+
+    private boolean IsCameraAvailable() {
+
+        PackageManager packageManager = getPackageManager();
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        List<ResolveInfo> list = packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        return list.size() > 0;
     }
 }
